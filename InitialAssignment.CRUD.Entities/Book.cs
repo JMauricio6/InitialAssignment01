639f06778fd48a4f1a1862f54ad36edd95d37285
JMauricio6 <jmauriciorivera@gmail.com>
2023-01-05 23:30:46 -0600

    Creacion de la Entidad Book

---
+++ InitialAssignment.CRUD.Entities/Book.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InitialAssignment.CRUD.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Autor")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Author { get; set; } = null!;
+
+        [Display(Name = "Clasificación")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Classification { get; set; } = null!;
+
+        [Display(Name = "Edición")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Edition { get; set; }
+
+        [Display(Name = "Editorial")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Editorial { get; set; } = null!;
+
+        [Display(Name = "Titulo")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Title { get; set; } = null!;
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Fecha de Publicación")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime PublicationDate { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+}
